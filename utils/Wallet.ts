7d79a0fcfd31cce8cd5b026dd5d4eaffa36acd39
Mazen Bilbeisi <mazen.bilbeisi@publicissapient.com>
2024-02-25 12:33:54 +0100

    maintain Wallet.ts

--- utils/Wallet.ts
+++ utils/Wallet.ts
@@ -1,9 +1,7 @@
 import Web3 from 'web3';
 
-// Initialize Web3 instance with the network RPC URL
 const web3 = new Web3('https://sepolia.infura.io/v3/2afe580f3e47456e8ea7a4d1bee3d063');
 
-// Define the structure of an account object for better type checking
 interface Account {
     address: string;
     privateKey: string;
@@ -16,18 +14,18 @@ class Wallet {
         this.web3 = web3Instance;
     }
 
-    generate(): Account {
+    generateWallet(): Account {
         const account = this.web3.eth.accounts.create();
         console.log(`Address: ${account.address}`);
         console.log(`Private Key: ${account.privateKey}`);
         return account;
     }
 
-    async getBalance(address: string): Promise<string> {
+    async getWalletBalance(walletAddress: string): Promise<string> {
         try {
-            const balanceWei = await this.web3.eth.getBalance(address);
+            const balanceWei = await this.web3.eth.getBalance(walletAddress);
             const balanceEther = this.web3.utils.fromWei(balanceWei, 'ether');
-            console.log(`Balance for ${address}: ${balanceEther} ETH`);
+            console.log(`Balance for ${walletAddress}: ${balanceEther} ETH`);
             return balanceEther;
         } catch (error) {
             console.error("Error fetching balance:", error);
@@ -36,4 +34,9 @@ class Wallet {
     }
 }
 
-export { Wallet, web3 };
+async function main(): Promise<void> {
+    const wallet = new Wallet(web3);
+    const account = wallet.generateWallet();
+}
+
+main().catch(error => console.error(error));
--- wallet/main.ts
+++
@@ -1,9 +0,0 @@
-import { Wallet, web3 } from '../utils/Wallet';
-
-async function main(): Promise<void> {
-    const wallet = new Wallet(web3);
-    const account = wallet.generate();
-    await wallet.getBalance('0x282A0c3e4Ce5066C8979E6Ef0d4393b094E03876');
-}
-
-main().catch(error => console.error(error));
